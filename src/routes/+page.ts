b3ea872eab602bde9690fc6191418d1738a664d0
Arnaud Ferrand <arferrand@gmail.com>
2026-04-22 22:28:25 +0100

    Adds Palestine, Vatican city and updates title

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,9 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
-  const title = 'Munoc 2025 - Globe';
-  const site_name = 'Munoc 2025 - Globe';
-  const site_description = 'This is the Munoc 2025 3d globe.';
+  const title = 'Munoc 2026 - Globe';
+  const site_name = 'Munoc 2026 - Globe';
+  const site_description = 'This is the Munoc 2026 3d globe.';
 
   return {
     title,
